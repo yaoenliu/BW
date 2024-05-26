25155ce28dbc2bc8d8f5b80dd4fd982171fb4abb
0423brian <130418761+0423brian@users.noreply.github.com>
2024-05-26 15:43:33 +0800

    game.cpp 新增

---
+++ game.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Hand()
+{
+
+}
+int main(){
+	string status = null;
+	cin >> status >> endl;
+	while (status == "Start")
+	{
+		string hand
+		// 輸入是否要再玩一次
+		cin >> status >> endl;
+	}
+}
